7e853300e128a79fbf9b98c871e870044642d3d4
RafaelOliveira1503 <rafael_tf@outlook.com>
2025-05-29 16:34:32 -0300

    criou a table

---
+++ db.js
@@ -0,0 +1,22 @@
+const {Client} = require('pg')
+
+
+const client = new Client ({
+    user:'postgres',
+    host:'localhost',
+    database: 'dc_store',
+    password: '1234',
+    port: 5432
+})
+
+// CREATE TABLE  IF NOT EXISTS cart (
+//  id SERIAL PRIMARY KEY,
+//  user_id INTEGER,
+// create_at TIMESTAMP DEFAULT CURRENT TIMESPTAMP);
+
+// CREATE TABLE IF NOT EXISTS cart_item (
+// id SERIAL PRIMARY KEY,
+// cart_id INTEGER REFERENCES cart(id) ON DELETE CASCADE),
+// product_id INTEGER,
+// quantity INTEGER DEFAULT 1
+//);
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cartRoute = require('/src/routes/cartRoute.js')
+
+const app = express();
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+
+})
+
+app.get('/boas-vindas', (req, res) => {
+    res.send('Seja Bem-vindo')
+})
+
+app.use('carrinho', cartRoute);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na url http://locashost:${port}`);
+    
+})
